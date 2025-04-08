6bfc903c2286883fb750cf2799b4cc8e130dc190
MadelonZwerink <madelonzwerink@hotmail.com>
2025-04-08 18:17:23 +0200

    Updated file system

---
+++ pupil-infants/pupil-infants/config.py
@@ -0,0 +1,5 @@
+
+from pathlib import Path  
+
+data_dir = Path('/Users/madel/OneDrive/Documenten/BiBC/ADS_stage/pupil-dilation-analysis/data')
+data_eyetracking_path = data_dir / 'eyetracking/' 
---
+++ pupil-infants/setup.py
@@ -0,0 +1,5 @@
+
+from setuptools import setup, find_packages
+
+setup(name="pupil-dilation-analysis",
+      version="0.1")
